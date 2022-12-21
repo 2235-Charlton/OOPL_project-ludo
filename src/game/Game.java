5c62afd6094e01760beaad78d44b42c58d4ed068
badmunda07 <avinashradha3035@gmail.com>
2022-12-21 15:12:56 +0530

    To print dice value

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,6 +2,8 @@ package game;
 
 import java.util.Scanner;
 
+import dice.Dice;
+
 public class Game {
 	public Game() {
 		Scanner in = new Scanner(System.in);
@@ -12,12 +14,16 @@ public class Game {
 			players = in.nextInt();
 		}
 		
+		Dice dice = new Dice();
+		int value;
+
 		while (choice !=9) {
 			System.out.println("Press 1 to roll the dice \nPress 9 to exit.");
 			choice = in.nextInt();
 			
 			if (choice == 1) {
-				
+				value = dice.rollDice();
+				System.out.println("Dice value: "+value);
 			}
 			else if (choice == 9) {
 				System.out.println("Quitting...");
