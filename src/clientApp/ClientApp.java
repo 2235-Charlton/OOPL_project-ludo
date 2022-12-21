9bacb6a98ee089d0d73d82d604ea9e0997f02bd0
Stanford <diasstanford>
2022-12-21 12:15:41 +0530

    added dice

--- src/clientApp/ClientApp.java
+++ src/clientApp/ClientApp.java
@@ -1,13 +1,8 @@
 package clientApp;
 
-import board.Board;
-
 public class ClientApp {
 
 	public static void main(String[] args) {
-		Board test= new Board();
-		
-		test.printBoard();
-//		test.update();
+
 	}
 }
