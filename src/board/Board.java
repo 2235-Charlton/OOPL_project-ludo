698e1a59cd1814c286fd3d6a8b5ebd5cdd1c9ace
2235-Charlton <mca.2235@unigoa.ac.in>
2022-12-22 04:54:42 +0530

    Added token movement order

--- src/board/Board.java
+++ src/board/Board.java
@@ -31,6 +31,12 @@ public class Board {
 			"E11", "F11", "G11",
 	};
 	private Map<String, Path> blocks = new HashMap<String, Path>();
+	private String movementOrder[][] = {
+		{"B5","C5","D5","E5","E4","E3","E2","E1","F1","G1","G2","G3","G4","G5","H5","I5","J5","K5","K6","K7","J7","I7","H7","G7","G8","G9","G10","G11","F11","E11","E10","E9","E8","E7","D7","C7","B7","A7","A6","B6","C6","D6","E6","F6"},
+		{"G2","G3","G4","G5","H5","I5","J5","K5","K6","K7","J7","I7","H7","G7","G8","G9","G10","G11","F11","E11","E10","E9","E8","E7","D7","C7","B7","A7","A6","A5","B5","C5","D5","E5","E4","E3","E2","E1","F1","F2","F3","F4","F5","F6"},
+		{"J7","I7","H7","G7","G8","G9","G10","G11","F11","E11","E10","E9","E8","E7","D7","C7","B7","A7","A6","A5","B5","C5","D5","E5","E4","E3","E2","E1","F1","G1","G2","G3","G4","G5","H5","I5","J5","K5","K6","J6","I6","H6","G6","F6"},
+		{"E10","E9","E8","E7","D7","C7","B7","A7","A6","A5","B5","C5","D5","E5","E4","E3","E2","E1","F1","G1","G2","G3","G4","G5","H5","I5","J5","K5","K6","K7","J7","I7","H7","G7","G8","G9","G10","G11","F11","F10","F9","F8","F7","F6"},
+	};
 
 	public Board() { // Initializing the board
 		margins();
@@ -237,6 +243,9 @@ public class Board {
 
 	public Map<String, Path> getBlocks() {
 		return blocks;
+	}
+	public String[][] order(){
+		return movementOrder;
 	}
 
 	public void update() {
