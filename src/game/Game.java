80fb6dd77602ad55aab4aa8e2458b298857a8b64
Stanford <diasstanford>
2022-12-22 15:02:54 +0530

    added player turns

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import board.Board;
@@ -9,48 +10,45 @@ import playerToken.PlayerToken;
 public class Game {
 	public Game() {
 		Scanner in = new Scanner(System.in);
-		int choice=0, players=0;
+		int choice = 0, players = 0;
 		System.out.println("------------Ludo Game-----------");
-		while( players >4 || players <2) {
+		while (players > 4 || players < 2) {
 			System.out.println("Enter the no. of players[2-4]:");
 			players = in.nextInt();
 		}
 
-		
-		
 		Board board = new Board();
-		String[] pathMap =  board.getBlockNames();
-		
-		PlayerToken player1 = new PlayerToken(pathMap,0);
-		PlayerToken player2 = new PlayerToken(pathMap,1);
-		
-		if(players<=4 && players>2){
-			PlayerToken player3 = new PlayerToken(pathMap,2);
-		}
-		if(players==4){
-			PlayerToken player4 = new PlayerToken(pathMap,3);
-		}
-		
+		String[] pathMap = board.getBlockNames();
+
+		ArrayList<PlayerToken> player = new ArrayList<PlayerToken>();
+
+		for (int i = 0; i < players; i++)
+			player.add(new PlayerToken(pathMap, i));
+
 		// board.printBoard();
 		Dice dice = new Dice();
-		int value;
+		int value, turn = 0;
 
-		while (choice !=9) {
-			System.out.println("Press 1 to roll the dice \nPress 9 to exit.");
+		while (choice != 9) {
+			System.out.println("Turn: player " + (turn + 1));
+
+			System.out.print("Press 1 to roll the dice \nPress 9 to exit.");
 			choice = in.nextInt();
 			
 			if (choice == 1) {
 				value = dice.rollDice();
-				System.out.println("Dice value: "+value);
-			}
-			else if (choice == 9) {
+				System.out.println("Dice value: " + value);
+			} else if (choice == 9) {
 				System.out.println("Quitting...");
-			}
-			else 
+			} else
 				System.out.println("Incorrect Option.");
+
+			if (turn == players - 1)
+				turn = 0;
+			else
+				turn++;
+
 		}
-		
-		
-		
+
 	}
 }
