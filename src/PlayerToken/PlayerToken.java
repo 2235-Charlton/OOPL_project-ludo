4585023d0177c242eac302ba4abab29a55c0ee6f
2235-Charlton <mca.2235@unigoa.ac.in>
2022-12-23 02:29:53 +0530

    player movement

--- src/PlayerToken/PlayerToken.java
+++ src/PlayerToken/PlayerToken.java
@@ -1,43 +1,47 @@
 package playerToken;
 
 public class PlayerToken {
-	private Integer[] position = { -1, -1, -1, -1 };
+	private Integer[] position = new Integer[4];
 	private boolean isAtHome[] = new boolean[4];
 	private String home[] = new String[4];
-	private String path[] = new String[43];
+	private String path[] = new String[44];
 
 	public PlayerToken(String[] location, int player) {
 		for (int i = 0; i < 4; i++) {
+			this.position[i] = -1;
 			this.isAtHome[i] = true;
 			this.home[i] = location[i + (player * 4)];
 		}
 
 	}
 
-	public Integer[] getPositions(){ //returns all the piece location 
-	return this.position;
+	public Integer[] getPositions() { // returns all the piece location
+		return this.position;
 	}
 
-	public void setPosition(int piece, int value) {
-		if (isAtHome(piece) && value == 6) //get out of the house
+	public void setPosition(Integer piece, Integer value) {
+		if (isAtHome(piece) && value == 6) { // get out of the house
 			this.position[piece] = 0;
-		else if(value==0) //go back home
+			this.setAtHome(piece, false);
+		} else if (value == 0) { // go home
 			this.position[piece] = -1;
-		else //move forward
-			this.position[piece] += value;
-
+			this.setAtHome(piece, true);
+		} else if (value <= 6) { // move forward
+			Integer newVal = this.position[piece] + value;
+			position[piece] = newVal;
+		}
 	}
 
 	public boolean isAtHome(int piece) {
 		return this.isAtHome[piece];
 	}
 
-	public void setAtHome(boolean atHome, int piece) {
+	public void setAtHome(int piece, boolean atHome) {
 		this.isAtHome[piece] = atHome;
 	}
 
 	public void setPlayerPath(String path[][], Integer player) {
-		for (int i = 0; i < 43; i++)
+		for (int i = 0; i < 44; i++)
 			this.path[i] = path[player][i];
 	}
 }
--- src/game/Game.java
+++ src/game/Game.java
@@ -25,7 +25,7 @@ public class Game {
 
 		for (int i = 0; i < players; i++) {
 			player.add(new PlayerToken(pathMap, i)); // assign home
-			player.get(i).setPlayerPath(order, i);// assign path
+			player.get(i).setPlayerPath(order, i); // assign path
 		}
 
 		// board.printBoard();
@@ -34,34 +34,51 @@ public class Game {
 
 		while (choice != 9) {
 			board.printBoard();
-			
+
+			showPosition(player, players, pathMap, order);
 			System.out.println("Turn: player " + (turn + 1));
 
 			System.out.print("Press 1 to roll the dice \nPress 9 to exit.");
 			choice = in.nextInt();
-
 			if (choice == 1) {
 				value = dice.rollDice();
 				System.out.println("Dice value: " + value);
+
+				System.out.print("Select the token you want to move[");
+				Integer position[] = player.get(turn).getPositions();
+				Boolean[] movable = { false, false, false, false };
+				Boolean hasMovable = false;
+				for (int i = 0; i < 4; i++) {
+					if (player.get(turn).isAtHome(i) && value == 6) {
+						movable[i] = true;
+						hasMovable = true;
+					} else if (player.get(turn).isAtHome(i) == false && (position[i] + value) < 44) {
+						movable[i] = true;
+						hasMovable = true;
+					}
+				}
+
+				for (int i = 0; i < 4; i++) {
+					if (movable[i])
+						System.out.print(" " + (i + 1));
+				}
+				System.out.println(" ]:");
+
+				if (hasMovable) {
+					Integer token = in.nextInt();
+					while (token<=0 || token>4 || !movable[token-1]) {
+						System.out.println("Invalid selection!");
+						token = in.nextInt();
+					}
+					player.get(turn).setPosition((token-1), value);
+				}
+
+
 			} else if (choice == 9) {
 				System.out.println("Quitting...");
 				return;
 			} else
 				System.out.println("Incorrect Option.");
-
-
-			for (int i = 0; i < players; i++) { // displaying the player location
-				Integer temp[] = player.get(i).getPositions();
-				System.out.print("Player " + (i + 1) + "{");
-				for (int j = 0; j < 4; j++) {
-					if (temp[j] == -1)
-						System.out.print(" " + pathMap[i + (j * 4)]);
-					else
-						System.out.print(" " + order[i][j]);
-				}
-				System.out.println(" }");
-			}
-			System.out.println();
 
 			// rest of the logic goes here
 
@@ -73,5 +90,20 @@ public class Game {
 			}
 		}
 
+	}
+
+	private void showPosition(ArrayList<PlayerToken> player, Integer players, String[] pathMap, String[][] order) {
+		for (int i = 0; i < players; i++) { // displaying the player location
+			Integer temp[] = player.get(i).getPositions();
+			System.out.print("Player " + (i + 1) + "{");
+			for (int j = 0; j < 4; j++) {
+				if (temp[j] == -1) // position when the token/pieces are at home
+					System.out.print(" " + pathMap[j + (i * 4)]);
+				else // pieces are on the move
+					System.out.print(" " + order[i][temp[j]]);
+			}
+			System.out.println(" }");
+		}
+		System.out.println();
 	}
 }
